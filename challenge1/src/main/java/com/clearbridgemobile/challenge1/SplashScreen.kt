85b79995ff3dfb35c04f66ea901c57e0aecfe016
Prabhjot Kaur <prabhjotpannu78@gmail.com>
2021-12-06 12:22:56 -0500

    commit

--- challenge1/src/main/java/com/clearbridgemobile/challenge1/SplashScreen.kt
+++ challenge1/src/main/java/com/clearbridgemobile/challenge1/SplashScreen.kt
@@ -20,7 +20,7 @@ class SplashScreen :AppCompatActivity() {
             finish()
         }, 3000)
     }
-//ANOTHER METHOD
+//ANOTHER METHOD LOADING MAIN ACTIVITY
 //        setContentView(R.layout.activity_splash)
 //        Handler().postDelayed({
 //            // This method will be executed once the timer is over

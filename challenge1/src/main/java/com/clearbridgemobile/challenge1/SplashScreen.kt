d9b7b56f1637b254593f39a561ed238118f64780
Alexis <oleksii.tomin@gmail.com>
2022-02-02 23:52:13 -0500

    Add Splash Screen
    This change adds splash Screen with the design corresponding to
    requirements of Challenge 1:
    - Splash color is purple.
    - Icon is set similar to the attached animation.

--- challenge1/src/main/java/com/clearbridgemobile/challenge1/SplashScreen.kt
+++ challenge1/src/main/java/com/clearbridgemobile/challenge1/SplashScreen.kt
@@ -1,11 +1,29 @@
 package com.clearbridgemobile.challenge1
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SplashScreen() {
-
+    Box(
+        modifier = Modifier
+            .background(color = colorResource(id = R.color.purple_200))
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_live_tv_48),
+            contentDescription = "Logo",
+        )
+    }
 }
 
 @Preview

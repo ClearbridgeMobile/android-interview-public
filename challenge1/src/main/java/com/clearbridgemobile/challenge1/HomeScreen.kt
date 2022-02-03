1abd938361a2ff25ca0420562824a36532103ace
Alexis <oleksii.tomin@gmail.com>
2022-02-02 23:54:29 -0500

    Add Home Screen
    This change adds Home Screen with the design corresponding to
    requirements of Challenge 1:
    - Home Screen is White.
    - UI elements are similar to the given animation.

--- challenge1/src/main/java/com/clearbridgemobile/challenge1/HomeScreen.kt
+++ challenge1/src/main/java/com/clearbridgemobile/challenge1/HomeScreen.kt
@@ -1,11 +1,37 @@
 package com.clearbridgemobile.challenge1
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen() {
-
+    Box(
+        modifier = Modifier
+            .background(colorResource(id = R.color.white))
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(
+                space = dimensionResource(id = R.dimen.spacer),
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = stringResource(id = R.string.home_hi))
+            Text(text = stringResource(id = R.string.home_time).format(getCurrentTime()))
+        }
+    }
 }
 
 @Preview

e6c8ec92aacafbd3128bc5a6a8acc527eb0ac174
Alexis <oleksii.tomin@gmail.com>
2022-02-02 23:53:03 -0500

    Add Welcome Screen
    This change adds Welcome Screen with the design corresponding to
    requirements of Challenge 1:
    - Splash color is Teal.
    - UI elements are set similar to the given animation.

--- challenge1/src/main/java/com/clearbridgemobile/challenge1/WelcomeScreen.kt
+++ challenge1/src/main/java/com/clearbridgemobile/challenge1/WelcomeScreen.kt
@@ -1,11 +1,49 @@
 package com.clearbridgemobile.challenge1
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WelcomeScreen() {
-
+    Box(
+        modifier = Modifier
+            .background(colorResource(id = R.color.teal_200))
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(
+                space = dimensionResource(id = R.dimen.spacer),
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = stringResource(id = R.string.welcome))
+            Text(text = stringResource(id = R.string.welcome_instructions))
+            Button(
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = colorResource(id = R.color.purple_200)
+                ),
+                onClick = {
+                    //TODO:
+                }
+            ) {
+                Text(text = stringResource(id = R.string.ok), color = colorResource(id = R.color.white))
+            }
+        }
+    }
 }
 
 @Preview
